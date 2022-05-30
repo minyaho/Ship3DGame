21a183bd6a3584174a73972a7bbac5922ebfbddc
Bill2015 <jimmy3112015@gmail.com>
2022-05-31 02:14:27 +0800

    Feat: add mechine gun sound effect

--- Assets/Scripts/Player/MechineGunShootController.cs
+++ Assets/Scripts/Player/MechineGunShootController.cs
@@ -5,28 +5,55 @@ using System;
 
 public class MechineGunShootController : MonoBehaviour
 {
-    public Transform aimTarget;
-    public bool IsAvailable = true;
-    public Transform mechineGunL;
-    public Transform mechineGunR;
-    public GameObject mechineGunBullet;
-    public float mechineGunDelay  = 0.5f;
-    private float shootDelay = 0.05f;
+    [Header("Sound Effect")]
+    public AudioSource miniGunWarmSound;            // 音效
+    public AudioSource miniGunFireSound;            // 音效
+    public AudioSource miniGunStopSound;            // 音效
+
+    [Header("Translate & GameObject")]
+    public Transform aimTarget;                 // 準心 (用來將不平整的發射點有個交會點)
+    public Transform mechineGunL;               // 左側發射點
+    public Transform mechineGunR;               // 右側發射點
+    public GameObject mechineGunBullet;         // 射出的 Object
+        
+    [Header("Delay")]
+    public float MmaxMechineGunDelay  = 0.1f;   // 最大 Minigun 間隔射速
+    public float maxWarmDelay = 0.5f;           // Minigun 暖機 (配合音效以及真實性)
+    private float warmDelay;  
+    private float shootDelay;
 
     void Start()
     {
-
+        warmDelay = maxWarmDelay;
+        shootDelay = MmaxMechineGunDelay;
     }
     void Update()
     {
         
         if( Input.GetMouseButton( 0 ) )
         {
-            if ( (shootDelay -= Time.deltaTime) <= 0) 
-            { 
-                Shoot();
-                shootDelay = mechineGunDelay; 
+            if( (warmDelay -= Time.deltaTime) <= 0 ){
+                if ( (shootDelay -= Time.deltaTime) <= 0) 
+                { 
+                    Shoot();
+                    shootDelay = MmaxMechineGunDelay; 
+                }
+                if( miniGunFireSound.isPlaying == false ){ miniGunFireSound.Play(); }
             }
+            if( miniGunWarmSound.isPlaying == false &&  miniGunFireSound.isPlaying == false ){  miniGunWarmSound.Play(); }
+        }
+        else
+        {
+            if( miniGunWarmSound.isPlaying || miniGunFireSound.isPlaying ){  
+                miniGunStopSound.Play();
+                miniGunStopSound.volume = 0.55f;
+                StartCoroutine(FadeAudioSource.StartFade(miniGunStopSound, 0.9f, 0));
+            }
+            warmDelay = maxWarmDelay;
+            shootDelay = MmaxMechineGunDelay; 
+            miniGunFireSound.Stop();
+            miniGunWarmSound.Stop();
+
         }
     }
 
@@ -38,5 +65,24 @@ public class MechineGunShootController : MonoBehaviour
         bullectL.gameObject.transform.LookAt(aimTarget);
         bullectR.gameObject.transform.LookAt(aimTarget);
 
+    }
+
+    IEnumerator VolumeFade(AudioSource _AudioSource, float _EndVolume, float _FadeLength)
+    {
+
+        float _StartVolume = _AudioSource.volume;
+
+        float _StartTime = Time.time;
+
+        while (Time.time < _StartTime + _FadeLength)
+        {
+
+            _AudioSource.volume = _StartVolume + ((_EndVolume - _StartVolume) * ((Time.time - _StartTime) / _FadeLength));
+
+            yield return null;
+
+        }
+
+        if (_EndVolume == 0) { _AudioSource.Stop(); }
     }
 }
