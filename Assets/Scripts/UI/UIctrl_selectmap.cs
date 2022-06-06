c9d4fed53eb0da69359f5e7dbd9fbaffaf0dc612
Steve-YLPeng <s60807@gmail.com>
2022-06-06 20:27:40 +0800

    update select_map UI

--- Assets/Scripts/UI/UIctrl_selectmap.cs
+++ Assets/Scripts/UI/UIctrl_selectmap.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class UIctrl_selectmap : MonoBehaviour
 {
     public GameObject panelCurrent;
-    public Button map1, map2, map3, btnReturn;
+    public Button map1, map2, map3,map4, btnReturn;
     // Start is called before the first frame update
     
     void Start()
@@ -15,6 +15,7 @@ public class UIctrl_selectmap : MonoBehaviour
         map1.onClick.AddListener(loadmap1);
         map2.onClick.AddListener(loadmap2);
         map3.onClick.AddListener(loadmap3);
+        map4.onClick.AddListener(loadmap4);
         btnReturn.onClick.AddListener(clickReturn);
     }
 
@@ -29,14 +30,18 @@ public class UIctrl_selectmap : MonoBehaviour
     }
     private void loadmap1(){
         Debug.Log("[Log] map");
-        SceneManager.LoadScene("Environment");
+        SceneManager.LoadScene("Environment_Horan_ver_1");
     }
     private void loadmap2(){
         Debug.Log("[Log] map");
-        SceneManager.LoadScene("NightEnvironment");
+        SceneManager.LoadScene("NightEnvironment_Horan_ver_1");
     }
     private void loadmap3(){
         Debug.Log("[Log] map");
         SceneManager.LoadScene("Tutorial");
+    }
+    private void loadmap4(){
+        Debug.Log("[Log] map");
+        SceneManager.LoadScene("Island");
     }
 }
